cf88afbc32c80ee84f890a6b0e669f77589cbdb1
Rebecca C. Nelson <dekarrin.irl@gmail.com>
2015-10-19 08:00:53 -0500

    Performing merge in temp repo

--- .travis/push_mainline.sh
+++ .travis/push_mainline.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo $TRAVIS_BRANCH
-[ "$GH_TOKEN" != "" ] && echo "fine"
-echo $TRAVIS_REPO_SLUG
 [ "$TRAVIS_BRANCH" != "dev" ] && return 0
 
-short_commit=$(git rev-parse --short HEAD)
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git fetch
-git reset --hard
-git checkout master --force
+short_commit=$(git rev-parse --short HEAD)
+
+# get entire repo
+repo_temp="$(mktemp -d)"
+
+git clone "https://github.com/$TRAVIS_REPO_SLUG" "$repo_temp"
+cd "$repo_temp"
+git checkout master
 git merge --no-ff -m "Merged commit $short_commit of dev into master after successful build" dev
-git push -q $GH_TOKEN@github.com:$TRAVIS_REPO_SLUG.git master
+git push -q "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" master
