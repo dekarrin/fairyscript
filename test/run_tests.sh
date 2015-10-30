15053a6c973ad1987e8c49a7154a3b30c9bd265b
Rebecca C. Nelson <dekarrin.irl@gmail.com>
2015-10-30 10:34:45 -0500

    Removed testing with word so we can close #9

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python scrappy.py -i test/full_test.scp -o test.rpy
-python scrappy.py -i test/full_test.scp -o test.docx --word
+#python scrappy.py -i test/full_test.scp -o test.docx --word
