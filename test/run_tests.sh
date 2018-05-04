dbc31617a66b0c904e593480f8ddd1eefbc48aaf
Rebecca C. Nelson <dekarrin.irl@gmail.com>
2018-05-04 10:06:00 -0500

    Test installed version and non-installed version

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+#
 ./scpcompile.py -i test/full_test.scp -o test.rpy
 ./scpcompile.py -i test/full_test.scp -o test.docx --word
+
+# test installed version as well
+scpcompile -i test/full_test.scp -o test.rpy
+scpcompile -i test/full_test.scp -o test.docx --word
